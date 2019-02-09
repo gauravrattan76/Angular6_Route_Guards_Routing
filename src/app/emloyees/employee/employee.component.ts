45d3b1fab2cbe6e72036f4f26c7b953c6fabfbaa
gaurav rattan <gauravrattan76@gmail.com>
2019-02-09 20:13:02 +0530

    updating changes for parammap subscribe

--- src/app/emloyees/employee/employee.component.ts
+++ src/app/emloyees/employee/employee.component.ts
@@ -1,34 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {employeeService} from '../../employeeService';
-import {Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { employeeService } from '../../employeeService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  providers:[employeeService],
+  providers: [employeeService],
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
 
-  emp:any;
-  id:string;
-  errorMessage:any;
-  constructor(private _activatedRoute:ActivatedRoute,private _employeeService:employeeService,private _router:Router) { }
+  emp: any;
+  id;
+  errorMessage: any;
+  constructor(private _activatedRoute: ActivatedRoute, private _employeeService: employeeService, private _router: Router) { }
+
+  // ngOnInit() {
+  //   this.id = this._activatedRoute.snapshot.paramMap.get('id');
+  //   return this._employeeService.getEmploye(this.id).subscribe(
+  //     (data) => this.emp = data,
+  //     (error) => this.errorMessage = error
+  //   )
+  // }
 
   ngOnInit() {
-     this.id = this._activatedRoute.snapshot.paramMap.get('id');
-   return this._employeeService.getEmploye(this.id).subscribe(
-     (data) => this.emp = data,
-     (error) => this.errorMessage = error
-   )
-
-
+    this._activatedRoute.params.subscribe(params => {
+      this.id = +params['id'];
+      return this._employeeService.getEmploye(this.id).subscribe(
+            (data) => this.emp = data,
+            (error) => this.errorMessage = error
+           )
+    });
   }
 
-  
-  onClick():void{
-     this._router.navigate(['/list']);
+
+  onClick(): void {
+    this._router.navigate(['/list']);
   }
-    
+
+  nextEmployee(){
+    console.log(this.id);
+    this.id = parseInt(this.id) + 1;
+    console.log(this.id);
+    this._router.navigate(['/list',this.id]);
+  }
+
 }
